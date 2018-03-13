946e131160099ba67dc5eaf3e4d3e996985fb59b
Azargul Nazari <nazariazargul@gmail.com>
2018-03-13 14:59:06 +0300

    Update and rename phpLibraries to phpLibraries.php

---
+++ public_html/phpLibraries.php
@@ -0,0 +1,39 @@
+
+<?php
+
+    class Math{}
+    
+    class File{}
+    
+    class Date {}
+    
+    class Database {}
+    
+    class Calander {}
+    
+    class Exception {}
+    
+    class Mail {}
+    
+    class FormFilter {}
+    
+    class XMLParse  {}
+    
+    class Ajax {}
+    
+    class Zip {}
+    
+    class TimeZone {}
+    
+    class FTP {}
+    
+    class ArrayList {}
+    
+    class LinkedList {}
+    
+    class HashTable {}
+    
+    
+        
+        
+        
